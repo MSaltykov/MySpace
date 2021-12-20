f8c8351436f151f99d456589271138c881bcb951
Skuld <skuld@list.ru>
2021-12-20 23:46:32 +0300

    Задача 4. Угол между стрелками часов.

--- src/test/java/test/TestExam_03.java
+++ src/test/java/test/TestExam_03.java
@@ -12,8 +12,20 @@ public class TestExam_03 {
     public static void main(String[] args) {
         System.out.println("Введите значение в градусах Цельсия");
         Scanner console = new Scanner(System.in);
-        double Cels = console.nextDouble();
-        System.out.println(Cels + "°C" + " = " + (Cels * 9 / 5 + 32) + "°F" + " = " + (Cels + 273.15) + "K");
+        double cels = console.nextDouble();
+        System.out.println(cels + "°C" + " = " + (cels * 9 / 5 + 32) + "°F" + " = " + (cels + 273.15) + "K");
+
+        System.out.println("Введите значение в градусах Фаренгейта");
+        //Scanner console = new Scanner(System.in);
+        double fahr = console.nextDouble();
+        System.out.println(fahr + "°F" + " = " + ((fahr - 32) * 5.0 / 9) + "°C" + " = " + (((fahr - 32) * 5.0 / 9)+273.15)
+        +"K");
+
+        System.out.println("Введите значение в Кельвинах");
+        //Scanner console = new Scanner(System.in);
+        double kelv = console.nextDouble();
+        System.out.println(kelv + "K" + " = " + ((kelv - 273.15) * 9 / 5 + 32)  + "°F" + " = " + (kelv - 273.15) + "°C");
+
     }
 }
 
