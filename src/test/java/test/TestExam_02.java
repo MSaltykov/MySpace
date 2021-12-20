c1e60d8950cf2969106a9e30f6d2e7b885dc41d3
Skuld <skuld@list.ru>
2021-12-20 23:22:24 +0300

    Задача 2. Поиск повторяющейся буквы

---
+++ src/test/java/test/TestExam_02.java
@@ -0,0 +1,19 @@
+package test;
+
+/**
+ * Написать программу, которая должна найти и вывести повторяющийся символ в слове «Hello»
+ */
+
+
+public class TestExam_02 {
+    public static void main(String[] args) {
+        String str = "Hello";
+        char[] array = str.toCharArray();
+        for (int i = 0; i < array.length; i++) {
+            for (int k = i + 1; k < array.length; k++) {
+                if (array[i] == array[k])
+                    System.out.println("повторяющаяся буква" + " " + array[k]);
+            }
+        }
+    }
+}
