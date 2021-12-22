23328852d24509f12733bce361c6b5983306a7b8
Skuld <skuld@list.ru>
2021-12-22 05:16:09 +0300

    ...

--- src/test/java/test/TestExam_04.java
+++ src/test/java/test/TestExam_04.java
@@ -13,14 +13,24 @@ public class TestExam_04 {
         Scanner console = new Scanner(System.in);
         int hours = console.nextInt();
         double minutes = console.nextInt();
-        if (hours > 23) System.out.println("Неверное значение часов");
+        if (hours > 23 || hours < 0) System.out.println("Неверное значение часов");
         if (hours > 12) hours = (hours - 12);
-        if (minutes > 59) System.out.println("Неверное значение минут");
+        if (minutes > 59 || minutes < 0) System.out.println("Неверное значение минут");
         /**
          * Этот метод рассчитывает смещение часовой стрелки в течение часа
          */
         double h = ((hours * 30) + ((minutes / 60) * 6));
+        if (hours == 12) h = ((minutes / 60) * 6);
         double m = minutes * 6;
         System.out.println("Угол между часовой и минутной стрелками составляет " + (Math.abs(h - m)) + " градусов");
+        /**
+         * Если нужно посчитать угол между стрелками часов, то тут немного по-другому
+         */
+        double x = h - m;
+        double y = 360 - (m - h);
+        double z = 0;
+        if (Math.abs(x) > Math.abs(y)) z = y;
+        else z = x;
+        System.out.println("Угол между стрелками часов составляет " + Math.abs(z));
     }
 }
